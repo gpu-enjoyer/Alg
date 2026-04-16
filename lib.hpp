5d8f028b7c58f33c53ec0be56fb56fb3a7edaf40
my_name <my_email>
2026-04-16 21:14:31 +0300

    getTime() refact., more templates

--- lib.hpp
+++ lib.hpp
@@ -9,13 +9,15 @@ using namespace std;
 using namespace chrono;
 
 
+template <typename T>
 bool check(
-    const vector<size_t>& a)
+    const vector<T>& a)
 {
     for (size_t i = 0; i < a.size() - 1; ++i)
         if (a[i] > a[i + 1]) return false;
     return true;
 }
+
 
 template <typename T>
 void gen_rand(
@@ -28,6 +30,17 @@ void gen_rand(
     for (size_t i = 0; i < a.size(); ++i)
 		a[i] = mt() % range;
 }
+template <typename T>
+void gen_rand(
+    vector<T>& a,
+    size_t size)
+{
+    mt19937 mt(time(nullptr));
+    a.resize(size);
+    for (size_t i = 0; i < a.size(); ++i)
+		a[i] = mt();
+}
+
 
 template <typename T>
 ostream& operator<<(
@@ -40,26 +53,55 @@ ostream& operator<<(
     return os;
 }
 
-template <typename SortFunc, typename... Args>
-double getMyTime(
-    SortFunc       mySort,
-    vector<size_t> a,
-    Args...        args)
+
+template <
+    typename  SortFunc,
+    typename  T,
+    typename... Args
+>
+double getTime(
+    SortFunc   sort_func,
+    vector<T>  a,
+    Args...    args)
 {
 	auto time0 = steady_clock::now();
-	if (!mySort(a, args...)) 
+	if (!sort_func(a, args...)) 
         throw std::runtime_error("Wrong sort");
 	auto time1 = steady_clock::now();
 	duration<double> time = time1 - time0;
 	return time.count();
 }
 
-double getStdTime(
-    vector<size_t> a)
-{
-	auto time0 = steady_clock::now();
-	sort(a.begin(), a.end());
-	auto time1 = steady_clock::now();
-	duration<double> time = time1 - time0;
-	return time.count();
-}
+
+
+
+// getStdTime(std::sort, a)
+// getStdTime(std::stable_sort, a)
+// template <
+//     typename SortFunc,
+//     typename T
+// >
+// double getStdTime(
+//     SortFunc  sort_func,
+//     vector<T> a)
+// {
+//     return getTime(sort_func, a, a.begin(), a.end());
+// }
+
+
+
+// template<string, T>
+// struct Line
+// {
+// 	string _txt  = "txt";
+// 	T      _var  = 0;
+// 	Line(string t, T v): _txt(t), _var(v) {}
+// 	Line() = default;
+// };
+
+// struct Message
+// {
+// 	int   l_num = 4;
+// 	Line* lines = new Line[l_num];
+//  Message();
+// };

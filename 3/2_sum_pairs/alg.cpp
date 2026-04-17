561eb9f0efafcd5139e7fb9be8ba72e7d4935f32
my_name <my_email>
2026-04-17 11:10:12 +0300

    3/2 done

--- 3/2_sum_pairs/alg.cpp
+++ 3/2_sum_pairs/alg.cpp
@@ -1,17 +1,100 @@
 
 #include "../../lib.hpp"
 
-// O(n + n*log(n)) = O(n*log(n))
+typedef unsigned long ulong;
 
-bool   merge_sort(); // n log(n)
-size_t count_sums(); // n
+
+// O(n + n*log(n)) = O(n*log(n))
+void __merge(
+	vector<ulong> &a,
+    vector<ulong> &buf,
+	size_t left,
+	size_t middle,
+	size_t right)
+{
+	size_t i(left), j(middle), k(left);
+
+	while (i < middle && j < right)
+	{
+		if (a[i] <= a[j]) buf[k++] = a[i++];
+		else              buf[k++] = a[j++];
+	}
+	while (i < middle)    buf[k++] = a[i++];
+	while (j < right)     buf[k++] = a[j++];
+
+	for (int t = left; t < right; ++t)
+		a[t] = buf[t];
+}
+
+// O(n * log(n))
+void __merge_sort(
+    vector<ulong>& a,
+    vector<ulong>& buf,
+    size_t left,
+    size_t right)
+{
+    // [left, right)
+    if (right - left <= 1) return;
+
+    size_t mid = left + (right - left) / 2;
+
+    __merge_sort(a, buf, left, mid);
+    __merge_sort(a, buf, mid,  right);
+            __merge(a, buf, left, mid, right);
+}
+
+bool merge_sort(
+    vector<ulong>& a)
+{
+    vector<ulong> buf(a);
+    __merge_sort(a, buf, 0, a.size());
+    if (!check(a)) return false;
+    return true;
+}
+
+void find_sums(
+    vector<ulong>& a,
+    ulong sum)
+{
+    ulong i     = 0;
+	ulong j     = a.size() - 1;
+
+	bool  found = false;
+
+	while (i < j)
+	{
+		ulong _sum = a[i] + a[j];
+
+		if (_sum == sum)
+		{
+			cout << ' ' << a[i] << " " << a[j] << "\n";
+			found = true;
+			++i;
+			--j;
+		}
+		else if (_sum < sum) ++i;
+		else                 --j;
+	}
+
+	if (!found)
+		cout << " no pairs\n";
+}
 
 
 int main()
 {
-    vector<size_t> a;
-    gen_rand_unique(a, 10, 20);
-    cout << a << '\n';
+    vector<ulong> a;
+    gen_rand_unique(a, 10, 200);
+
+    ulong sum = 0;
+    
+    cout << "       before sort: " << a << '\n';
+    merge_sort(a);
+    cout << "        after sort: " << a << '\n';
+    cout << "               sum: ";
+    cin >> sum;
+    cout << "count_sums(a, sum): " << '\n';
+    find_sums(a, sum);
 
     return 0;
 }
